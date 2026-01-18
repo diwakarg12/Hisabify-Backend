5c1185972dd240a424321f744cc4fd338c71ab39
Diwakar Giri <diwakargiri23@gmail.com>
2026-01-18 19:23:00 +0530

    feat: added vercel.json file to enable us to host the application on vercel
    Fix:
    1. updated the db.js code so that node should not send connection request again and again if the DB is connected
    2. update app.js to remove app.listen bcs vercel is does not support this

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+dotenv.config();
+
 const authRouter = require('../src/routes/authRouter');
 const profileRouter = require('./routes/profileRouter');
 const groupRouter = require('./routes/groupRouter');
 const inviteRouter = require('./routes/inviteRouter');
 const expenseRouter = require('./routes/expenseRouter');
 const messageRouter = require('./routes/messageRouter');
-const PORT = process.env.PORT || 3000;
-dotenv.config();
+
 const app = express();
 
 app.use(express.json({ limit: "10mb" }));
@@ -18,7 +19,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
-}))
+}));
 
 
 
@@ -29,12 +30,21 @@ app.use('/invite', inviteRouter);
 app.use('/expense', expenseRouter);
 app.use('/message', messageRouter)
 
+connectDB();
 
-connectDB().then(() => {
-    console.log('Database connected successfully');
-    app.listen(PORT, () => {
-        console.log(`Server is running on PORT: ${PORT}`);
-    })
-}).catch((err) => {
-    console.log(`Error while Connecting to DB: ${err}`);
-});
+module.exports = app;
+
+
+
+//We can do this if the platform where we have to deploy the application is not serverless
+
+// const PORT = process.env.PORT || 3000;
+
+// connectDB().then(() => {
+//     console.log('Database connected successfully');
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on PORT: ${PORT}`);
+//     })
+// }).catch((err) => {
+//     console.log(`Error while Connecting to DB: ${err}`);
+// });
