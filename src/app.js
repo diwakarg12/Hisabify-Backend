ee0050e437dee7faa5807dbf28009a9a13d99e8f
Diwakar Giri <diwakargiri23@gmail.com>
2026-05-12 01:18:17 +0530

    resolved cors error and also implemented reset password logic

--- src/app.js
+++ src/app.js
@@ -18,17 +18,26 @@ const app = express();
 
 // const allowedOrigins = process.env.CLIENT_URL.split(",");
 const allowedOrigins = [
-    "https://hisabify-app.vercel.app",
-    "http://localhost:5173",
+    'http://localhost:5173',
+    'https://hisabify-app.vercel.app'
 ];
-
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) return callback(null, true);
+        return callback(null, false);
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 let isConnected = false;
 
