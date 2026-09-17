7c7c4e29e6fe5cdb2e04641b3cf1bd37b9a784aa
Diwakar Giri <diwakargiri23@gmail.com>
2026-09-17 19:21:56 +0530

    feat: add Express application setup with CORS, routes, and database connection

--- src/app.js
+++ src/app.js
@@ -46,8 +46,8 @@ const corsOptions = {
             return callback(null, true);
         }
 
-        // Allow any localhost / 127.0.0.1 port (for local dev flexibility)
-        if (/^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin)) {
+        // Allow any localhost, 127.0.0.1, or local Wi-Fi IP (192.168.x.x, 10.x.x.x, 172.x.x.x)
+        if (/^http:\/\/(localhost|127\.0\.0\.1|192\.168\.\d+\.\d+|10\.\d+\.\d+\.\d+|172\.(1[6-9]|2\d|3[01])\.\d+\.\d+)(:\d+)?$/.test(origin)) {
             return callback(null, true);
         }
 
